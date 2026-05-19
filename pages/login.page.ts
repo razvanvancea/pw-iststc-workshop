d244c6b0421ec55c4cd6e4e160f870a1b3c40ee7
Razvan Vancea <razvanvancea94@gmail.com>
2026-05-19 08:11:44 +0300

    remove commented code

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -25,14 +25,3 @@ export class LoginPage {
     await expect(this.loggedInUsername).toContainText(expectedUsername);
   }
 }
-
-// await page.goto('https://practicesoftwaretesting.com/');
-// await page.locator('[data-test="nav-sign-in"]').click();
-// await page.locator('[data-test="email"]').click();
-// await page.locator('[data-test="email"]').fill('admin@practicesoftwaretesting.com');
-// await page.locator('[data-test="password"]').click();
-// await page.locator('[data-test="password"]').fill('welcome01');
-// await page.locator('[data-test="password"]').click();
-// await page.locator('[data-test="login-submit"]').click();
-// await expect(page.locator('[data-test="nav-menu"]')).toContainText('John Doe');
-// await page.locator('div').nth(3).click();
